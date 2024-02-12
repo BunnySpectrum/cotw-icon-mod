4c3a302e9c1bdf4d0b37364538624766725c005f
BunnySpectrum <bunnyspectrum@gmail.com>
2024-02-11 22:39:22 -0800

    adding basic text draws with key press

--- main/editor/iconedit.c
+++ main/editor/iconedit.c
@@ -1,5 +1,18 @@
-#include <WINDOWS.H>
+#include <WINDOWS.H>  
+#include <stdio.h>
 
+
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+
+#ifndef WIN31                       
+#define FAR  
+#define PASCAL  
+#define _export
+typedef char* LPSTR;
+typedef const char* LPCSTR;
+#endif                                  
+                                    
 long FAR PASCAL _export WndProcMain(HWND, UINT, UINT, LONG);
 long FAR PASCAL _export WndProcToolbar(HWND, UINT, UINT, LONG);
 long FAR PASCAL _export WndProcCanvas(HWND, UINT, UINT, LONG);
@@ -8,6 +21,7 @@ long FAR PASCAL _export WndProcLog(HWND, UINT, UINT, LONG);
 char szNameToolbar[] = "Toolbar";
 char szNameCanvas[] = "Canvas";
 char szNameLog[] = "Log";
+LPSTR buffer = "1234567890";
 
 int PASCAL WinMain(HANDLE hInstance, HANDLE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow){
     static char szNameApp[] = "CharacterCreator";
@@ -79,11 +93,26 @@ long FAR PASCAL _export WndProcMain(HWND hwnd, UINT message, UINT wParam, LONG l
     static HWND hwndToolbar;
     static HWND hwndCanvas;
     static HWND hwndLog;
-    short cxBlock, cyBlock;
-
+    short cxBlock, cyBlock, canvasSize;
+    static short counter;
+    char szBuffer[40];
+    short nLength;
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
+    static short cxChar, cxCaps, cyChar;
+    TEXTMETRIC tm;
 
     switch(message){
         case WM_CREATE:
+            counter = 0;
+            hdc = GetDC(hwnd);
+            GetTextMetrics(hdc, &tm);
+            cxChar = tm.tmAveCharWidth;
+            cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
+            cyChar = tm.tmHeight + tm.tmExternalLeading;
+            ReleaseDC(hwnd, hdc);
+
             hwndToolbar = CreateWindow(szNameToolbar, NULL, WS_CHILDWINDOW | WS_VISIBLE,
                                         0, 0, 0, 0,
                                         hwnd, 1, GetWindowWord(hwnd, GWW_HINSTANCE), NULL);
@@ -101,20 +130,64 @@ long FAR PASCAL _export WndProcMain(HWND hwnd, UINT message, UINT wParam, LONG l
         case WM_SIZE:
             cxBlock = LOWORD(lParam) / 5;
             cyBlock = HIWORD(lParam) / 5;
+            canvasSize = min(cxBlock<<2, cyBlock<<2);
 
-            MoveWindow(hwndToolbar, 0, 0, cxBlock, cyBlock<<2, TRUE);
-            MoveWindow(hwndCanvas, cxBlock, 0, cxBlock<<2, cyBlock<<2, TRUE);
+            MoveWindow(hwndToolbar, 0, cyBlock, cxBlock, cyBlock*3, TRUE);
+            // MoveWindow(hwndToolbar, 0, 0, cxBlock, cyBlock<<2, TRUE);
+            MoveWindow(hwndCanvas, cxBlock, 0, canvasSize, canvasSize, TRUE);
             MoveWindow(hwndLog, 0, cyBlock<<2, LOWORD(lParam), cyBlock, TRUE);
-            MessageBeep(0);   
+            MessageBeep(0);  
             return 0;
 
         case WM_LBUTTONDOWN:
-            MessageBeep(0);
+            MessageBeep(MB_OK);
+
             return 0;
+        
+        case WM_KEYDOWN:
+            // nLength = wsprintf(szBuffer, "wParam %d", wParam);
+            // MessageBox(hwnd, szBuffer, "WM_KEYDOWN", MB_ICONEXCLAMATION | MB_OK);
+            
+            switch(wParam){
+                case VK_LEFT:
+                    MessageBeep(MB_OK);
+                    // nLength = wsprintf(szBuffer, "wParam %d", wParam);
+                    nLength = wsprintf(szBuffer, "Left %d %d", wParam, counter);
+    
+                    hdc = GetDC(hwnd);
+                    TextOut(hdc, cxChar/2, cyChar/2, szBuffer, nLength);
+                    ReleaseDC(hwnd, hdc);
+                    ValidateRect(hwnd, NULL);
+                    break;
+
+                case VK_RIGHT:
+                    MessageBeep(MB_ICONASTERISK);
+                    nLength = wsprintf(szBuffer, "Right %d %d", message, counter);
+
+                    GetClientRect(hwndLog, &rect);   
+                    hdc = GetDC(hwndLog);
+                    TextOut(hdc, cxChar/2 + rect.left, cyChar/2 + rect.top, szBuffer, nLength);
+                    ReleaseDC(hwndLog, hdc);
+                    ValidateRect(hwndLog, NULL);
+                    break;
+            }
+            return 0;
+        
+        case WM_PAINT:
+                    nLength = wsprintf(szBuffer, "Paint %d, %d", counter++, 80);
+
+                    hdc = BeginPaint(hwnd, &ps);
+                    TextOut(hdc, 5, 80, szBuffer, nLength);
+                    EndPaint(hwnd, &ps);
+                    return 0;
 
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
+
+        default:
+            ;
+
     }
     return DefWindowProc(hwnd, message, wParam, lParam);
 }
@@ -133,15 +206,13 @@ long FAR PASCAL _export WndProcToolbar(HWND hwnd, UINT message, UINT wParam, LON
         case WM_PAINT:
             hdc = BeginPaint(hwnd, &ps);
             GetClientRect(hwnd, &rect);   
+
+            Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
+            MoveTo(hdc, rect.left, rect.top);
+            LineTo(hdc, rect.right, rect.bottom);
+            MoveTo(hdc, rect.left, rect.bottom);
+            LineTo(hdc, rect.right, rect.top);
             
-            MoveTo(hdc, 0, 0);
-            LineTo(hdc, rect.right, 0);
-            LineTo(hdc, rect.right, rect.bottom);
-            LineTo(hdc, 0, rect.bottom);
-            LineTo(hdc, 0, 0);
-            LineTo(hdc, rect.right, rect.bottom);
-            MoveTo(hdc, 0, rect.bottom);
-            LineTo(hdc, rect.right, 0);
 
             DrawText(hdc, "Toolbar", -1, &rect, 
                         DT_SINGLELINE | DT_CENTER | DT_VCENTER);
@@ -165,17 +236,20 @@ long FAR PASCAL _export WndProcCanvas(HWND hwnd, UINT message, UINT wParam, LONG
             hdc = BeginPaint(hwnd, &ps);
             GetClientRect(hwnd, &rect);
             
-            MoveTo(hdc, 0, 0);
-            LineTo(hdc, rect.right, 0);
-            LineTo(hdc, rect.right, rect.bottom);
-            LineTo(hdc, 0, rect.bottom);
-            LineTo(hdc, 0, 0);
-            LineTo(hdc, rect.right, rect.bottom);
-            MoveTo(hdc, 0, rect.bottom);
-            LineTo(hdc, rect.right, 0);
+            Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
             
-            DrawText(hdc, "Canvas", -1, &rect, 
-                        DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            MoveTo(hdc, rect.left, rect.top);
+            LineTo(hdc, rect.right, rect.bottom);
+
+            MoveTo(hdc, rect.left, rect.bottom);
+            LineTo(hdc, rect.right, rect.top);
+
+
+
+
+            
+//            DrawText(hdc, "Canvas", -1, &rect, 
+  //                      DT_SINGLELINE | DT_CENTER | DT_VCENTER);
             EndPaint(hwnd, &ps);
             return 0;
     }
@@ -196,14 +270,12 @@ long FAR PASCAL _export WndProcLog(HWND hwnd, UINT message, UINT wParam, LONG lP
             hdc = BeginPaint(hwnd, &ps);
             GetClientRect(hwnd, &rect);
             
-            MoveTo(hdc, 0, 0);         
-            LineTo(hdc, rect.right, 0);
+            Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
+            MoveTo(hdc, rect.left, rect.top);
             LineTo(hdc, rect.right, rect.bottom);
-            LineTo(hdc, 0, rect.bottom);
-            LineTo(hdc, 0, 0);
-            LineTo(hdc, rect.right, rect.bottom);
-            MoveTo(hdc, 0, rect.bottom);
-            LineTo(hdc, rect.right, 0);
+            MoveTo(hdc, rect.left, rect.bottom);
+            LineTo(hdc, rect.right, rect.top);
+
             
             DrawText(hdc, "Log", -1, &rect, 
                         DT_SINGLELINE | DT_CENTER | DT_VCENTER);
